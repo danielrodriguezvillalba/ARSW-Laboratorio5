2280ebae0104e230b1828e9197ac36890743afe6
Daniel Rodriguez <daniel.rodriguez-vi@mail.escuelaing.edu.co>
2019-09-23 20:10:18 -0500

    Correccion parte final

--- BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -1,7 +1,6 @@
 app= (function (){
     var funcModify = function (variable) {
-        if(variable){
-            document.getElementById("autorLabel").innerHTML = author;
+        if(variable != null){
             var arreglo = variable.map(function(blueprint){
                 return {key:blueprint.name, value:blueprint.points.length}
             })
@@ -11,11 +10,11 @@ app= (function (){
                 var temporal = '<tr>'+ '<td>' + blueprint.key + '</td><td>' + blueprint.value + '</td><td><input type="button" value="Open"></td></tr>';
                 $("#tabla tbody").append(temporal);
             })
-            var valorTotal = 0;
-            arreglo.map(function(blueprint){
-                valorTotal = valorTotal + blueprint.value;
-            })
 
+            var valorTotal = arreglo.reduce(function(total, valor){
+                return total.value + valor.value;
+            })
+            document.getElementById("autorLabel").innerHTML = author;
             document.getElementById("puntosLabel").innerHTML = valorTotal;
         }
     };
